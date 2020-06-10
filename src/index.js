0ecb4dbe64b4654143ad6fb3cace35dd2fa6463a
Avi vigdorchik <vigdora@macbook-pro.home>
2020-06-10 11:39:51 +0300

    remove reduntant files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Home from './home';
-import Demo from './Demo';
 import {Provider} from 'react-redux';
 import store from './store'
 
